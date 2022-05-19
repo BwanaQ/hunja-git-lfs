34688c7ac56e9a2fedab34096b4ead88e653eca2
BwanaQ <thunjawax@gmail.com>
2022-05-19 11:23:01 +0300

    remove images

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id) \
 AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key) \
-npx node-git-lfs
+npx "github:Dermah/node-git-lfs#4b79bee4"
